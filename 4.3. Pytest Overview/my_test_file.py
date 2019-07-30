1610f38dc298c3ecc1c5801b8981c53df6491741
Grey <sergejyurskyj@yahoo.com>
2019-07-29 22:10:00 -0700

    The second Commit

---
+++ 4.3. Pytest Overview/my_test_file.py
@@ -0,0 +1,14 @@
+class TestClass():
+    def test_me(self):
+        assert True
+
+    def test_me2(self):
+        assert True
+
+
+class MyTestClass():
+    def test_it(self):
+        assert True
+
+    def test_it2(self):
+        assert True
